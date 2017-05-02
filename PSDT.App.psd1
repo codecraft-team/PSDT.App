84c7dc66609e742351344f30be8a4dc1eebb7a3f
Jozsef Horvath <mail.jozsef.horvath@gmail.com>
2017-05-02 20:25:51 +0200

    PSDT.App: moving code from PSDevTools to separate repository.

---
+++ PSDT.App.psd1
@@ -0,0 +1,11 @@
+@{
+    RootModule = '.\PSDT.App.psm1'
+    ModuleVersion = '1.0.0.0'
+    GUID = '23256234-e648-4d9e-b2f8-799114ef9322'
+    Author = 'Tauri-Code'
+    CompanyName = 'Tauri-Code'
+    Copyright = '(c) 2017 Tauri-Code. All rights reserved.'
+    Description = 'A collection of PowerShell developer tool related basic cmdlets.'
+    FunctionsToExport = @("Get-Directory","Get-File","Find-ChildItem")
+    AliasesToExport = @("f","d","fci")
+}
---
+++ PSDT.App.psm1
@@ -0,0 +1,166 @@
+$host.ui.RawUI.WindowTitle = "PSDT | PowerShell Developer Tools";
+
+if (Test-Path Function:\TabExpansion) {
+	Rename-Item Function:\TabExpansion PreGetVSSolutionTabExpansion
+}
+
+Function global:TabExpansion($line, $lastWord) {
+    $filter = ($line -split " " | Select-Object -Skip 1) -join ".*";
+   
+  switch -regex ($line) {
+		"^(Get-VSSolution|gvss) .*" {
+      Get-VSSolution $filter | Select-Object -ExpandProperty FullName
+    }
+		"^(Get-File|f) .*" {
+      Get-File $filter | Select-Object -ExpandProperty FullName
+    }
+		"^(Get-Directory|d) .*" {
+      Get-Directory $filter | Select-Object -ExpandProperty FullName
+    }
+		default {
+			if (Test-Path Function:\PreGetVSSolutionTabExpansion) {
+				PreGetVSSolutionTabExpansion $line $lastWord
+			}
+		}
+	}
+}
+
+$global:PSDTChildItemCache = @{};
+$global:PSDTChildItemCacheFiltered = @{};
+
+Function Find-ChildItem {
+  Param (
+    [string]$Filter = "*.*",
+    [switch]$File,
+    [switch]$Directory
+  )
+
+  $fullNameFilter = $args -join ".*";
+  $fullNameFilter = ".*{0}.*" -f $fullNameFilter;
+  $itemCache = $global:PSDTChildItemCache;
+  $itemCacheFilterd = $global:PSDTChildItemCacheFiltered;
+
+  $key = "$((Get-Location).Path)-File:$File-Directory:$Directory-Filter:$Filter";
+  $keyOfFilteredItems = "$key-$fullNameFilter";
+  
+  If(-not $itemCache.ContainsKey($key)) { 
+      $searchProgressActivity = "Find child items...";
+      
+      Write-Progress -Activity $searchProgressActivity -Status $fullNameFilter;
+      $childItems = (Get-ChildItem .\ -Recurse -Filter $Filter -File:$File -Directory:$Directory);
+      Write-Progress -Activity $searchProgressActivity -Completed;
+      
+      $itemCache.Add($key, $childItems); 
+  }
+
+  If($args.Length -gt 0 -and [System.IO.Path]::IsPathRooted($args[$args.Length - 1])) {
+      return Get-Item $args[$args.Length - 1];
+  }
+  
+  If(-not $itemCacheFilterd.ContainsKey($keyOfFilteredItems)) {
+      $filteredItems = $itemCache[$key] | Where-Object { ($_.FullName -match $fullNameFilter) };
+      $itemCacheFilterd.Add($keyOfFilteredItems, $filteredItems);
+  }
+
+  return $itemCacheFilterd[$keyOfFilteredItems];
+}
+
+Set-Alias fci Find-ChildItem;
+
+<#
+.Synopsis
+    Gets all files under the current path, which match the filter parameters.
+    The cmdlet's default alias is: f
+.DESCRIPTION
+    The cmdlet is using an internal cache, which is stored in the script scope.
+.EXAMPLE
+    The example gets all files on the drive R, where the FullName matches the pattern *common*full*.sln*.
+   
+    PS R:\Get-File common full .sln
+   
+        Directory: R:\Source\cool-project\master\common\Sources\Builds\FullBuild
+
+    Mode                LastWriteTime         Length Name                                                                                           
+    ----                -------------         ------ ----                                                                                           
+    -a----         1/9/2017   8:03 AM          21870 FullBuild.sln                                                                                  
+
+.EXAMPLE
+    The example shows all matching files using tab completion.
+
+    Type the command below and press the TAB.
+        
+        PS R:\Get-File common full .sln
+
+    Pressing the tab will replace the last word with the first matching file.
+        
+        PS R:\Get-File common full R:\Source\cool-project\master\common\Sources\Builds\FullBuild.sln
+
+.EXAMPLE
+    The example shows all matching files in a completion list.
+
+    After typing the following line:
+        
+        PS R:\Get-File common f .sln
+
+    Pressing the CTRL+SPACE will replace the last word with the first matching file's full name, and will list each other matches.
+        
+        PS R:\Get-File common R:\Source\cool-project\master\common\Sources\Builds\FullBuild.sln
+        R:\Source\cool-project\master\common\Sources\Builds\FeedbackBuild.sln
+        R:\Source\cool-project\master\common\Sources\Builds\FeatureBuild.sln
+
+#>
+Function Get-File {
+  return Find-ChildItem -Filter "*.*" -File @args;
+}
+
+Set-Alias f Get-File;
+
+<#
+.Synopsis
+    Gets all directories under the current path, which match the filter parameters.
+    The cmdlet's default alias is: d
+.DESCRIPTION
+    The cmdlet is using an internal cache, which is stored in the script scope.
+.EXAMPLE
+    The example gets all directories on the drive R, where the FullName matches the pattern *common*full*.sln*.
+   
+    PS R:\Get-Directory common full
+   
+        Directory: R:\Source\cool-project\master\common\Sources\Builds
+
+    Mode                LastWriteTime         Length Name                                                                                           
+    ----                -------------         ------ ----                                                                                           
+    d-----         1/9/2017   8:03 AM          21870 FullBuild                                                                                  
+
+.EXAMPLE
+    The example shows all matching directories using tab completion.
+
+    Type the command below and press the TAB.
+        
+        PS R:\Get-Directory common full
+
+    Pressing the tab will replace the last word with the first matching directory.
+        
+        PS R:\Get-Directory common full R:\Source\cool-project\master\common\Sources\Builds\FullBuild
+
+.EXAMPLE
+    The example shows all matching directories in a completion list.
+
+    After typing the following line:
+        
+        PS R:\Get-Directory common f
+
+    Pressing the CTRL+SPACE will replace the last word with the first matching directory's full name, and will list each other matches.
+        
+        PS R:\Get-Directory common R:\Source\cool-project\master\common\Sources\Builds\FullBuild
+        R:\Source\cool-project\master\common\Sources\Features
+        R:\Source\cool-project\master\common\Sources\Features\Feature1
+        R:\Source\cool-project\master\common\Sources\Features\Feature1\Project1
+        R:\Source\cool-project\master\common\Sources\Features\Feature2
+
+#>
+Function Get-Directory {
+  return Find-ChildItem -Filter "*.*" -Directory @args;
+}
+
+Set-Alias d Get-Directory;
